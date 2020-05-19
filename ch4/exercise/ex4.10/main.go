fcf5d3902d6faff9dd4baee04b6d96096f2b7b62
wedojava <wedojava@gmail.com>
2020-05-19 21:01:33 +0800

    The best prectice of exercise 4.10

--- ch4/exercise/ex4.10/main.go
+++ ch4/exercise/ex4.10/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"gopl.io/ch4/github"
 )
@@ -14,8 +15,47 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
-	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+	FmtResult(result)
+}
+
+func FmtResult(r *github.IssuesSearchResult) {
+	now := time.Now()
+	format := "#%-5d %9.9s %.55s\n"
+
+	oneMonth := now.AddDate(0, -1, 0)
+	oneYear := now.AddDate(-1, 0, 0)
+
+	rsLessMonth := make([]*github.Issue, 0)
+	rsLessYear := make([]*github.Issue, 0)
+	rsGreaterYear := make([]*github.Issue, 0)
+
+	for _, i := range r.Items {
+		switch {
+		case i.CreatedAt.After(oneMonth) || i.CreatedAt.Equal(oneMonth):
+			rsLessMonth = append(rsLessMonth, i)
+		case i.CreatedAt.Before(oneMonth) && i.CreatedAt.After(oneYear):
+			rsLessYear = append(rsLessYear, i)
+		case i.CreatedAt.Before(oneYear):
+			rsGreaterYear = append(rsGreaterYear, i)
+		}
+	}
+
+	fmt.Println("[!] Issues less than a month:\n-----------------------------")
+	if len(rsLessMonth) > 0 {
+		for _, i := range rsLessMonth {
+			fmt.Printf(format, i.Number, i.User.Login, i.Title)
+		}
+	}
+	fmt.Println("[!] Issuses less than a year:\n-----------------------------")
+	if len(rsLessYear) > 0 {
+		for _, i := range rsLessYear {
+			fmt.Printf(format, i.Number, i.User.Login, i.Title)
+		}
+	}
+	fmt.Println("[!] Issues more than a year:\n-----------------------------")
+	if len(rsGreaterYear) > 0 {
+		for _, i := range rsGreaterYear {
+			fmt.Printf(format, i.Number, i.User.Login, i.Title)
+		}
 	}
 }
