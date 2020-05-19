c374152a7f92c057d49f0a39dc204d15c140cdcf
wedojava <wedojava@gmail.com>
2020-05-20 00:55:44 +0800

    cp from torbiak/gopl

---
+++ ch4/exercise/ex4.11/main.go
@@ -0,0 +1,147 @@
+// Build a tool lets users CRUD Github issues from CLI,
+// invoke there prefered text editor when substantial text input is required.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+
+	"gopl.io/ch4/github"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		usageDie()
+	}
+	cmd := os.Args[1]
+	args := os.Args[2:]
+	if cmd == "search" {
+		if len(args) < 1 {
+			usageDie()
+		}
+		search(args)
+		os.Exit(0)
+	}
+	if len(args) != 3 {
+		usageDie()
+	}
+	owner, repo, number := args[0], args[1], args[2]
+	switch cmd {
+	case "read":
+		_read(owner, repo, number)
+	case "edit":
+		_edit(owner, repo, number)
+	case "close":
+		_close(owner, repo, number)
+	case "open":
+		_open(owner, repo, number)
+	}
+}
+
+var usage string = `
+usage:
+search QWERY
+[read|edit|close|open] OWNER REPO ISSUE_NUMBER
+`
+
+func usageDie() {
+	fmt.Fprintln(os.Stderr, usage)
+	os.Exit(1)
+}
+
+func search(query []string) {
+	result, err := github.SearchIssues(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	format := "#%-5d %9.9s %.55s\n"
+	for _, i := range result.Items {
+		fmt.Printf(format, i.Number, i.User.Login, i.Title)
+	}
+}
+
+func _read(owner, repo, number string) {
+	issue, err := github.GetIssue(owner, repo, number)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body := issue.Body
+	if body == "" {
+		body = "<empty>\n"
+	}
+	fmt.Printf("repo: %s/%s\nnumber: %s\nuser: %s\ntitle: %s\n\n%s",
+		owner, repo, number, issue.User.Login, issue.Title, body)
+}
+
+func _edit(owner, repo, number string) {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vim"
+	}
+	editorPath, err := exec.LookPath(editor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tempfile, err := ioutil.TempFile("", "issue_crud")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tempfile.Close()
+	defer os.Remove(tempfile.Name())
+
+	issue, err := github.GetIssue(owner, repo, number)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	encoder := json.NewEncoder(tempfile)
+	err = encoder.Encode(map[string]string{
+		"title": issue.Title,
+		"state": issue.State,
+		"body":  issue.Body,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := &exec.Cmd{
+		Path:   editorPath,
+		Args:   []string{editor, tempfile.Name()},
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tempfile.Seek(0, 0)
+	fields := make(map[string]string, 0)
+	if err = json.NewDecoder(tempfile).Decode(&fields); err != nil {
+		log.Fatal(err)
+	}
+	_, err = github.EditIssue(owner, repo, number, fields)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func _close(owner, repo, number string) {
+	_, err := github.EditIssue(owner, repo, number, map[string]string{"state": "closed"})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func _open(owner, repo, number string) {
+	_, err := github.EditIssue(owner, repo, number, map[string]string{"state": "open"})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- ch4/github/github.go
+++ ch4/github/github.go
@@ -3,15 +3,13 @@
 package github
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"net/url"
-	"strings"
 	"time"
 )
 
-const IssuesURL = "https://api.github.com/search/issues"
+const (
+	APIURL    string = "https://api.github.com"
+	IssuesURL        = "https://api.github.com/search/issues"
+)
 
 type IssuesSearchResult struct {
 	TotalCount int `json:"total_count"`
@@ -31,27 +29,4 @@ type Issue struct {
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
-}
-
-// SearchIssues queries the Github issue tracker.
-func SearchIssues(terms []string) (*IssuesSearchResult, error) {
-	q := url.QueryEscape(strings.Join(terms, " "))
-	resp, err := http.Get(IssuesURL + "?q=" + q)
-	if err != nil {
-		return nil, err
-	}
-
-	// We must close resp.Body on all execution paths.
-	// (Chapter 5 presents 'defer', which makes this simpler.)
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("search query failed: %s", resp.Status)
-	}
-	var result IssuesSearchResult
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		resp.Body.Close()
-		return nil, err
-	}
-	resp.Body.Close()
-	return &result, nil
 }
