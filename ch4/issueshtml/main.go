b125ae6bc92dc79f681170cea9e625f168e97d6c
wedojava <wedojava@gmail.com>
2020-05-23 00:26:38 +0800

    study over

--- ch4/issueshtml/main.go
+++ ch4/issueshtml/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"html/template"
+	"log"
+	"os"
+
+	"gopl.io/ch4/github"
 )
 
-func main() {
-	var issueList = template.Must(template.New("issuelist").Parse(`
+var issueList = template.Must(template.New("issuelist").Parse(`
 	<h1>{{.TotalCount}} issues</h1>
 	<table>
 	    <tr>
@@ -24,4 +27,13 @@ func main() {
 	    {{end}}
 	</table>
 	`))
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := issueList.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
 }
