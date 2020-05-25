7efd32eb110929613fbdd1ae67e2bb02555d1372
wedojava <wedojava@gmail.com>
2020-05-26 02:19:32 +0800

    exercise 5.2 done.

--- ch5/exercise/ex5.2/main.go
+++ ch5/exercise/ex5.2/main.go
@@ -1,8 +1,35 @@
 // Write a function to populate a mapping
 // from element names -- p, div, span, and so on --
 // to the number of elements with that name in an HTML document tree.
+// go run gopl.io/ch1/fetch https://golang.org | go run main.go
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ex5.2: err: %v\n", err)
+		os.Exit(1)
+	}
+	for k, v := range visit(make(map[string]int), doc) {
+		fmt.Printf("%s: %d\n", k, v)
+	}
+}
+
+func visit(counts map[string]int, n *html.Node) map[string]int {
+	if n == nil {
+		return counts
+	}
+	if n.Type == html.ElementNode {
+		counts[n.Data]++
+	}
+	visit(counts, n.FirstChild)
+	visit(counts, n.NextSibling)
+	return counts
 }
