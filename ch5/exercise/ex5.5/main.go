673602aac16ba31c2bedd23e733e9884637b3cd6
wedojava <wedojava@gmail.com>
2020-05-27 03:25:35 +0800

    study at 5.5

---
+++ ch5/exercise/ex5.5/main.go
@@ -0,0 +1,62 @@
+// Implement countWordsAndImages
+// go run main.go https://xkcd.com
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		words, images, err := CountWordsAndImages(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ch05/ex05: %v\n", err)
+			continue
+		}
+		fmt.Printf("words: %d\n", words)
+		fmt.Printf("images: %d\n", images)
+	}
+}
+
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	if n == nil {
+		return
+	}
+	if n.Type == html.TextNode {
+		words += len(strings.Fields(n.Data))
+	}
+	if n.Type == html.ElementNode && n.Data == "img" {
+		images++
+		// to indeed href is not null
+		// for _, a := range n.Attr {
+		//         if a.Key == "href" && a.Val != "" {
+		//                 images++
+		//         }
+		// }
+	}
+	cwords, cimages := countWordsAndImages(n.FirstChild)
+	swords, simages := countWordsAndImages(n.NextSibling)
+	words += cwords + swords
+	images += cimages + simages
+	return
+}
---
+++ ch5/exercise/ex5.5/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCountWordsAndImages(t *testing.T) {
+	var tests = []struct {
+		html string
+		want struct {
+			words  int
+			images int
+		}
+	}{}
+}
