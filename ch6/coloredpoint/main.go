bf52f76bd539152125a9cc434f3b5d48eb784486
wedojava <wedojava@gmail.com>
2020-06-11 01:14:40 +0800

    need to study and understand the intset e.g.

--- ch6/coloredpoint/main.go
+++ ch6/coloredpoint/main.go
@@ -35,12 +35,44 @@ func (p *Point) ScaleBy(factor float64) {
 	p.Y *= factor
 }
 
-func (p *Point) Distance(q Point) float64 {
+func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+// method expression
 func init() {
-	fmt.Println("init() start")
+	p := Point{1, 2}
+	q := Point{4, 6}
+	distance := Point.Distance  // method expression
+	fmt.Println(distance(p, q)) // 5
+	fmt.Printf("%T\n", distance)
+
+	scale := (*Point).ScaleBy
+	scale(&p, 2)
+	fmt.Println(p)
+	fmt.Printf("%T\n", scale)
+	fmt.Println("init1 ----------------------------")
+}
+
+// method value
+func init() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	distanceFromP := p.Distance   // method value
+	fmt.Println(distanceFromP(q)) // 5
+	var origin Point
+	fmt.Println(distanceFromP(origin)) // 2.23...
+
+	scaleP := p.ScaleBy // method value
+	scaleP(2)
+	scaleP(3)
+	scaleP(10)
+	fmt.Println(p)
+	fmt.Println("init 2----------------------------")
+}
+
+// ColoredPoint methods are promoted indirectly from *Point
+func init() {
 	red := color.RGBA{255, 0, 0, 255}
 	blue := color.RGBA{0, 0, 255, 255}
 
@@ -55,5 +87,5 @@ func init() {
 	q.Point = p.Point
 	p.ScaleBy(2)
 	fmt.Println(*p.Point, *q.Point) // {2 2} {2 2}
-	fmt.Println("init() over")
+	fmt.Println("init3 ----------------------------")
 }
