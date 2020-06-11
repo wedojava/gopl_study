16da16e2b11458147b8280a8d5f0c5d7e4717d11
wedojava <wedojava@gmail.com>
2020-06-12 07:29:29 +0800

    just study

--- ch7/exercise/ex7.01/counter.go
+++ ch7/exercise/ex7.01/counter.go
@@ -16,6 +16,7 @@ type WordCounter int
 
 func (c *WordCounter) Write(p []byte) (int, error) {
 	s := bufio.NewScanner(bytes.NewBuffer(p))
+	// Set the split function for the scanning operation.
 	s.Split(bufio.ScanWords)
 	var count int
 	for s.Scan() {
@@ -28,6 +29,7 @@ func (c *WordCounter) Write(p []byte) (int, error) {
 type LineCounter int
 
 func (c *LineCounter) Write(p []byte) (int, error) {
+	// NewScanner returns a new Scanner to read from r. The split function defaults to ScanLines.
 	s := bufio.NewScanner(bytes.NewBuffer(p))
 	var count int
 	for s.Scan() {
