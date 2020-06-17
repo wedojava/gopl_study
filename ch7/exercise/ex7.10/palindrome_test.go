9be9429d7a857852eee8c78d84e654ad2f5bc3f2
wedojava <wedojava@gmail.com>
2020-06-18 07:14:07 +0800

    copy and study done.

---
+++ ch7/exercise/ex7.10/palindrome_test.go
@@ -0,0 +1,25 @@
+package palindrome
+
+import (
+	"testing"
+)
+
+func TestPalindrome(t *testing.T) {
+	tcs := []struct {
+		s       []int
+		expects bool
+	}{
+		{[]int{1, 2, 3, 2, 1}, true},
+		{[]int{1, 2, 2, 1}, true},
+		{[]int{1, 1, 1}, true},
+		{[]int{1, 1}, true},
+		{[]int{1}, true},
+		{[]int{1, 2, 3}, false},
+	}
+	for _, tc := range tcs {
+		actual := IsPalindrome(nums(tc.s))
+		if actual != tc.expects {
+			t.Errorf("%v expects: %t, actual %t", tc.s, tc.expects, actual)
+		}
+	}
+}
