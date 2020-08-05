933f29cfc1793425c60d8339146d222658fa9763
wedojava <wedojava@gmail.com>
2020-08-06 00:55:33 +0800

    add

---
+++ ch2/popcount/popcount_test.go
@@ -0,0 +1,13 @@
+package popcount
+
+// -- Alternative implementations --
+func BitCount(x uint64) int {
+	// Hacker's Delight, Figure 5-2.
+	x = x - ((x >> 1) & 0x5555555555555555)
+	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+	x = x + (x >> 8)
+	x = x + (x >> 16)
+	x = x + (x >> 32)
+	return int(x & 0x7f)
+}
