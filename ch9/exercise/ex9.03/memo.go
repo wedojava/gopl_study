8ae4367b41ea47702277c232ebef07ab140f8f37
wedojava <wedojava@gmail.com>
2020-08-13 00:09:12 +0800

    75%

--- ch9/exercise/ex9.03/memo.go
+++ ch9/exercise/ex9.03/memo.go
@@ -1,16 +1,15 @@
 package memo
 
+import (
+	"errors"
+)
+
+//!+Func
+
+// Func is the type of the function to memoize.
 type Func func(key string) (interface{}, error)
 
-type Memo struct {
-	requests chan request
-}
-
-type request struct {
-	key      string
-	response chan<- result
-}
-
+// A result is the result of calling a Func
 type result struct {
 	value interface{}
 	err   error
@@ -18,45 +17,76 @@ type result struct {
 
 type entry struct {
 	res   result
-	ready chan struct{}
+	ready chan struct{} // closed when res is ready
 }
 
+//!-Func
+
+//!+get
+
+// A request is a message requesting that the Func be applied to key.
+type request struct {
+	key      string
+	response chan<- result // the client wants a single result
+	done     <-chan bool
+}
+
+type Memo struct{ requests chan request }
+
+// New returns a memoization of f. Clients must subsequently call Close.
 func New(f Func) *Memo {
 	memo := &Memo{requests: make(chan request)}
 	go memo.server(f)
 	return memo
 }
 
-func (memo *Memo) Get(key string) (interface{}, error) {
+func (memo *Memo) Get(key string, done <-chan bool) (interface{}, error) {
 	response := make(chan result)
-	memo.requests <- request{key, response}
+	memo.requests <- request{key, response, done}
 	res := <-response
 	return res.value, res.err
 }
 
 func (memo *Memo) Close() { close(memo.requests) }
 
+//!-get
+
+//!+monitor
+
 func (memo *Memo) server(f Func) {
 	cache := make(map[string]*entry)
 	for req := range memo.requests {
+		select {
+		case <-req.done:
+			delete(cache, req.key)
+			req.response <- result{nil, errors.New("canceled")}
+			continue
+		default:
+		}
 		e := cache[req.key]
 		if e == nil {
+			// This is the first request for this key.
 			e = &entry{ready: make(chan struct{})}
 			cache[req.key] = e
-			go e.call(f, req.key) // f(key)
+			go e.call(f, req.key) // call f(key)
 		}
 		go e.deliver(req.response)
 	}
 }
 
-// f(key)
+// call the function f(key)
 func (e *entry) call(f Func, key string) {
+	// Evaluate the function.
 	e.res.value, e.res.err = f(key)
 	close(e.ready)
 }
 
-// send result to response
+// deliver e.res to response channel
 func (e *entry) deliver(response chan<- result) {
+	// Wait for the ready condition.
 	<-e.ready
+	// Send the result to the client.
 	response <- e.res
 }
+
+//!-monitor
--- ch9/exercise/ex9.03/memo_test.go
+++ ch9/exercise/ex9.03/memo_test.go
@@ -1,57 +1,143 @@
-package memo_test
+package memo
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
 	"testing"
-
-	memo "gopl.io/ch9/exercise/ex9.03"
-	"gopl.io/ch9/memotest"
+	"time"
 )
 
-var httpGetBody = memotest.HTTPGetBody
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+var HTTPGetBody = httpGetBody
+
+func incomingURLs() <-chan string {
+	ch := make(chan string)
+	go func() {
+		for _, url := range []string{
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+		} {
+			ch <- url
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+type M interface {
+	Get(key string, done <-chan bool) (interface{}, error)
+}
+
+func Sequential(t *testing.T, m M) {
+	for url := range incomingURLs() {
+		start := time.Now()
+		value, err := m.Get(url, nil)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Printf("%s, %s, %d bytes\n",
+			url, time.Since(start), len(value.([]byte)))
+	}
+}
+
+func Concurrent(t *testing.T, m M) {
+	var n sync.WaitGroup
+	for url := range incomingURLs() {
+		n.Add(1)
+		go func(url string) {
+			defer n.Done()
+			start := time.Now()
+			value, err := m.Get(url, nil)
+			if err != nil {
+				log.Print(err)
+				return
+			}
+			fmt.Printf("%s, %s, %d bytes\n",
+				url, time.Since(start), len(value.([]byte)))
+		}(url)
+	}
+	n.Wait()
+}
 
 func Test(t *testing.T) {
-	m := memo.New(httpGetBody)
+	m := New(httpGetBody)
 	defer m.Close()
-	memotest.Sequential(t, m)
+	Sequential(t, m)
 }
 
-// NOTE: not concurrency-safe! Test fails.
 func TestConcurrent(t *testing.T) {
-	m := memo.New(httpGetBody)
+	m := New(httpGetBody)
 	defer m.Close()
-	memotest.Concurrent(t, m)
+	Concurrent(t, m)
 }
 
-// go test -run=TestConcurrent -race -v gopl.io/ch9/memo5
-// === RUN   TestConcurrent
-// https://godoc.org, 851.863413ms, 7522 bytes
-// https://godoc.org, 852.22799ms, 7522 bytes
-// https://golang.org, 862.925321ms, 11298 bytes
-// https://golang.org, 863.731648ms, 11298 bytes
-// https://play.golang.org, 1.331234063s, 6314 bytes
-// https://play.golang.org, 1.330243492s, 6314 bytes
-// http://gopl.io, 2.457548797s, 4154 bytes
-// http://gopl.io, 2.456314205s, 4154 bytes
-// --- PASS: TestConcurrent (2.46s)
-// PASS
-// ok	gopl.io/ch9/memo5   2.482s
+func TestCancel(t *testing.T) {
+	m := New(httpGetBody)
+	defer m.Close()
+	key := "https://golang.org"
+	wg1 := &sync.WaitGroup{}
+	wg1.Add(1)
+	go func() {
+		v, err := m.Get(key, nil)
+		wg1.Done()
+		if v == nil {
+			t.Errorf("got %v, %v; want %v, %v", v, err, nil, err)
+		}
+	}()
+	wg1.Wait()
+
+	wg2 := &sync.WaitGroup{}
+	wg2.Add(1)
+	go func() {
+		done := make(chan bool)
+		close(done)
+		v, err := m.Get(key, done)
+		if v != nil || err == nil {
+			t.Errorf("got %v, %v; want %v, %v", v, err, nil, "cancled")
+		}
+		wg2.Done()
+	}()
+	wg2.Wait()
+}
 
 // go test
-// https://golang.org, 977.655771ms, 11298 bytes
-// https://godoc.org, 1.015557021s, 7522 bytes
-// https://play.golang.org, 1.432728503s, 6314 bytes
-// http://gopl.io, 2.874637569s, 4154 bytes
-// https://golang.org, 9.556µs, 11298 bytes
-// https://godoc.org, 15.94µs, 7522 bytes
-// https://play.golang.org, 11.753µs, 6314 bytes
-// http://gopl.io, 10.162µs, 4154 bytes
-// https://godoc.org, 269.067261ms, 7522 bytes
-// https://godoc.org, 269.29707ms, 7522 bytes
-// https://golang.org, 283.143443ms, 11298 bytes
-// https://golang.org, 283.251154ms, 11298 bytes
-// http://gopl.io, 633.561869ms, 4154 bytes
-// http://gopl.io, 633.616549ms, 4154 bytes
-// https://play.golang.org, 1.076823934s, 6314 bytes
-// https://play.golang.org, 1.076942205s, 6314 bytes
+// https://golang.org, 1.288683603s, 7357 bytes
+// https://godoc.org, 2.384097906s, 7522 bytes
+// https://play.golang.org, 1.839392069s, 6314 bytes
+// http://gopl.io, 7.469410039s, 4154 bytes
+// https://golang.org, 5.869µs, 7357 bytes
+// https://godoc.org, 2.691µs, 7522 bytes
+// https://play.golang.org, 2.577µs, 6314 bytes
+// http://gopl.io, 2.051µs, 4154 bytes
+// https://golang.org, 282.159308ms, 7357 bytes
+// https://golang.org, 282.165141ms, 7357 bytes
+// https://godoc.org, 460.012123ms, 7522 bytes
+// https://godoc.org, 459.998389ms, 7522 bytes
+// https://play.golang.org, 782.364177ms, 6314 bytes
+// https://play.golang.org, 782.291101ms, 6314 bytes
+// http://gopl.io, 955.75384ms, 4154 bytes
+// http://gopl.io, 955.656183ms, 4154 bytes
 // PASS
-// ok	gopl.io/ch9/memo5   7.385s
+// ok  	gopl.io/ch9/exercise/ex9.03	16.352s
+
+// go test -run=TestCancel
+// PASS
+// ok  	gopl.io/ch9/exercise/ex9.03	1.367s
